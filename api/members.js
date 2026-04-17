19835ac9ecac3e48055812fb37466cfefd533046
natisfamq <puterek2@gmail.com>
2026-04-17 14:58:58 +0200

    ds

--- api/members.js
+++ api/members.js
@@ -1,38 +1,28 @@
 export default async function handler(req, res) {
     const hierarchy = [
-        "1435005337492263033", // 15 | Bosik
-        "1435005341606875300", // 14 | V-Lider
-        "1435005371105411215", // 13 | Prawa ręka
-        "1435005373198241945", // 12 | Lewe jądro
-        "1435005375727276112", // 11 | Klapmistrz
-        "1435005377090682881", // 10 | Main
-        "1435005379917385768", // 9  | Porucznik
-        "1435005393834344620", // 8  | Weteran
-        "1435005395759534192", // 7  | Hunter
-        "1435005397907013652", // 6  | Egzekutor
-        "1435005399978999828", // 5  | Torpeda
-        "1435005406924636241", // 4  | Młody
-        "1477020190393893035", // 3  | Jopek
-        "1435005408757551196", // 2  | Klapek
-        "1435005410913550397"  // 1  | Zawieszony
+        "1435005337492263033", "1435005341606875300", "1435005371105411215", 
+        "1435005373198241945", "1435005375727276112", "1435005377090682881", 
+        "1435005379917385768", "1435005393834344620", "1435005395759534192", 
+        "1435005397907013652", "1435005399978999828", "1435005406924636241", 
+        "1477020190393893035", "1435005408757551196", "1435005410913550397"
     ];
 
     const roleNames = {
-        "1435005337492263033": "Bosik",
-        "1435005341606875300": "V-Lider",
-        "1435005371105411215": "Prawa ręka",
-        "1435005373198241945": "Lewe jądro",
-        "1435005375727276112": "Klapmistrz",
-        "1435005377090682881": "Main",
-        "1435005379917385768": "Porucznik",
-        "1435005393834344620": "Weteran",
-        "1435005395759534192": "Hunter",
-        "1435005397907013652": "Egzekutor",
-        "1435005399978999828": "Torpeda",
-        "1435005406924636241": "Młody",
-        "1477020190393893035": "Jopek",
-        "1435005408757551196": "Klapek",
-        "1435005410913550397": "Zawieszony"
+        "1435005337492263033": "15 | Bosik",
+        "1435005341606875300": "14 | V-Lider",
+        "1435005371105411215": "13 | Prawa ręka",
+        "1435005373198241945": "12 | Lewe jądro",
+        "1435005375727276112": "11 | Klapmistrz",
+        "1435005377090682881": "10 | Main",
+        "1435005379917385768": "9 | Porucznik",
+        "1435005393834344620": "8 | Weteran",
+        "1435005395759534192": "7 | Hunter",
+        "1435005397907013652": "6 | Egzekutor",
+        "1435005399978999828": "5 | Torpeda",
+        "1435005406924636241": "4 | Młody",
+        "1477020190393893035": "3 | Jopek",
+        "1435005408757551196": "2 | Klapek",
+        "1435005410913550397": "1 | Zawieszony"
     };
 
     try {
@@ -46,7 +36,6 @@ export default async function handler(req, res) {
             .map(m => {
                 const topRoleId = hierarchy.find(r => m.roles.includes(r));
                 return {
-                    // Priorytet: Pseudonim na serwerze -> Nazwa wyświetlana -> Login
                     displayName: m.nick || m.user.global_name || m.user.username,
                     avatar: m.user.avatar ? `https://cdn.discordapp.com/avatars/${m.user.id}/${m.user.avatar}.png` : `https://cdn.discordapp.com/embed/avatars/${parseInt(m.user.id) % 5}.png`,
                     rankName: roleNames[topRoleId] || "Członek",
@@ -57,6 +46,6 @@ export default async function handler(req, res) {
 
         res.status(200).json(result);
     } catch (err) {
-        res.status(500).json({ error: "Błąd serwera podczas pobierania członków" });
+        res.status(500).json({ error: "Błąd serwera" });
     }
 }
