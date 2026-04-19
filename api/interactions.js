e840af2194b75fe1b83dfe2c74de8a2cc5e44535
natisfamq <puterek2@gmail.com>
2026-04-19 12:10:16 +0000

    dsds

--- api/interactions.js
+++ api/interactions.js
@@ -1,4 +1,7 @@
 export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        return res.status(200).json({ message: 'Interactions endpoint is alive.' });
+    }
     if (req.method !== 'POST') return res.status(405).end();
 
     const interaction = req.body;
